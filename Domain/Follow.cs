ceb5124f8d6b7b68019ffb443d32eeee21855fe7
Behnam <behnam.ebrahimi@gmail.com>
2020-04-16 18:29:47 +1000

    Update Follow.cs

--- Domain/Follow.cs
+++ Domain/Follow.cs
@@ -3,8 +3,8 @@ namespace Domain
     public class Follow
     {
         public string FollowerId { get; set; }
-        public virtual AppUser Follower { get; set; }
-        public string FollowingId { get; set; }
+        public AppUser Follower { get; set; }
+        public FollowingId { get; set; }
         public virtual AppUser Following { get; set; }
     }
 }
